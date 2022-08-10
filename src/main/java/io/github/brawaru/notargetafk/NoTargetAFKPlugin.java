56a885c3034ca9cebfe87d5add38e813c5aa6630
Sasha Sorokin <10401817+Brawaru@users.noreply.github.com>
2022-08-10 04:59:50 +0200

    feat: permissions and optimisations
    - Added permission for losing track of player ongoing AFK.
    - Added permission for not tracking player on AFK.
    - Remove all targets that tracked player on disconnect.
    - Refactoring: renaming variables and docs.

--- src/main/java/io/github/brawaru/notargetafk/NoTargetAFKPlugin.java
+++ src/main/java/io/github/brawaru/notargetafk/NoTargetAFKPlugin.java
@@ -13,6 +13,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.Nullable;
 
@@ -23,16 +24,16 @@ public final class NoTargetAFKPlugin extends JavaPlugin implements Listener {
     /**
      * A map of lists of mobs that target player, per player.
      * <p>
-     * This is used to remove targets
+     * This is used to remove targets.
      */
     private final Map<UUID, List<UUID>> playerTargetingEntities = new HashMap<>();
 
     /**
      * Reverse lookup map of mobs' targets.
      * <p>
-     * This is used to quickly lookup what player entity was previously targeting.
+     * Used to quickly lookup which player entity was previously targeting.
      */
-    private final Map<UUID, UUID> entitiesTargetPlayers = new HashMap<>();
+    private final Map<UUID, UUID> entitiesTargets = new HashMap<>();
 
     @Override
     public void onEnable() {
@@ -56,17 +57,17 @@ public final class NoTargetAFKPlugin extends JavaPlugin implements Listener {
         var target = e.getTarget();
 
         if (target instanceof Player player) {
-            if (essentials.getUser(player).isAfk()) {
+            if (essentials.getUser(player).isAfk() && target.hasPermission("notargetafk.mobs-dont-target")) {
                 e.setCancelled(true);
             } else {
                 var playerUuid = player.getUniqueId();
 
                 playerTargetingEntities.computeIfAbsent(playerUuid, (key) -> new ArrayList<>()).add(entityUuid);
 
-                entitiesTargetPlayers.put(entityUuid, playerUuid);
+                entitiesTargets.put(entityUuid, playerUuid);
             }
         } else {
-            var previouslyTargetedPlayerUuid = entitiesTargetPlayers.get(entityUuid);
+            var previouslyTargetedPlayerUuid = entitiesTargets.get(entityUuid);
 
             if (previouslyTargetedPlayerUuid != null) {
                 var targetedBy = playerTargetingEntities.get(previouslyTargetedPlayerUuid);
@@ -75,7 +76,30 @@ public final class NoTargetAFKPlugin extends JavaPlugin implements Listener {
                     targetedBy.remove(entityUuid);
                 }
 
-                entitiesTargetPlayers.remove(entityUuid);
+                entitiesTargets.remove(entityUuid);
+            }
+        }
+    }
+
+    private void clearTargetsForPlayer(Player player) {
+        var playerUuid = player.getUniqueId();
+
+        var targetingEntities = playerTargetingEntities.get(playerUuid);
+
+        if (targetingEntities != null && !targetingEntities.isEmpty()) {
+            for (var iterator = targetingEntities.listIterator(); iterator.hasNext(); ) {
+                var targetingEntityUuid = iterator.next();
+                var targetingEntity = Bukkit.getEntity(targetingEntityUuid);
+
+                if (targetingEntity instanceof Mob mob) {
+                    var mobTarget = mob.getTarget();
+
+                    if (mobTarget != null && mobTarget.getUniqueId().equals(playerUuid)) {
+                        mob.setTarget(null);
+
+                        iterator.remove();
+                    }
+                }
             }
         }
     }
@@ -85,32 +109,20 @@ public final class NoTargetAFKPlugin extends JavaPlugin implements Listener {
         if (e.getValue()) {
             var player = e.getAffected().getBase();
 
-            var playerUuid = player.getUniqueId();
-
-            var targetingEntities = playerTargetingEntities.get(playerUuid);
-
-            if (targetingEntities != null && !targetingEntities.isEmpty()) {
-                for (var iterator = targetingEntities.listIterator(); iterator.hasNext(); ) {
-                    var targetingEntityUuid = iterator.next();
-                    var targetingEntity = Bukkit.getEntity(targetingEntityUuid);
-
-                    if (targetingEntity instanceof Mob mob) {
-                        var mobTarget = mob.getTarget();
-
-                        if (mobTarget != null && mobTarget.getUniqueId().equals(playerUuid)) {
-                            mob.setTarget(null);
-
-                            iterator.remove();
-                        }
-                    }
-                }
+            if (player.hasPermission("notargetafk.mobs-lose-track")) {
+                clearTargetsForPlayer(player);
             }
         }
+    }
+
+    @EventHandler
+    private void onDisconnect(PlayerQuitEvent e) {
+        playerTargetingEntities.remove(e.getPlayer().getUniqueId());
     }
 
     @Override
     public void onDisable() {
         playerTargetingEntities.clear();
-        entitiesTargetPlayers.clear();
+        entitiesTargets.clear();
     }
 }
